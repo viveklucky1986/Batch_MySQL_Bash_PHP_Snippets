2c69ec66beb7d2652c156d3d1fb4a5038a68903e
viveklucky1986 <viveklucky1848@yahoo.in>
2015-10-30 09:59:10 +0530

    Update generic_scripts.bat

--- generic_scripts.bat
+++ generic_scripts.bat
@@ -1,1 +1,11 @@
-
+:: Restart Desktop Window Manager(Aero theme) ::
+rem Run as 'cmd /c "$(FULL_CURRENT_PATH)"'
+rem Run as 'cmd /k "$(FULL_CURRENT_PATH)"'
+::goto comment
+goto comment1
+echo Stopping Aero Service...
+net stop uxsms
+echo Starting Aero Service...
+net start uxsms
+pause
+:comment1
