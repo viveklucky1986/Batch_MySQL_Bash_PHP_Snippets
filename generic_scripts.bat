b4eac3ebb69410837dc6acf71c824bb2dbba0357
viveklucky1986 <viveklucky1848@yahoo.in>
2015-10-30 10:00:00 +0530

    Update generic_scripts.bat

--- generic_scripts.bat
+++ generic_scripts.bat
@@ -9,3 +9,24 @@ echo Starting Aero Service...
 net start uxsms
 pause
 :comment1
+
+:: Apply full permissions to any file/folder ::
+REM --v2 -----------------------------------------------------------
+takeown /r /d y /f %1
+icacls %1 /t /grant Everyone:F
+
+REM takeown /r /d y /f will set the owner to the Administrators group recursively.
+REM /t makes it recursive
+REM /grant Sets the permision to the following user, replaces existing permissions for the specified uesr.
+REM :f Grants full controll permission.
+REM ----------------------------------------------------------------
+
+REM --v1 -----------------------------------------------------------
+REM cacls %1 /t /e /g Everyone:f
+
+REM /t makes it recursive
+REM /e Edits the ACL instead of replaceing it
+REM /g Grants permissions to the following users
+REM :f Grants full controll permission.
+REM ----------------------------------------------------------------
+
