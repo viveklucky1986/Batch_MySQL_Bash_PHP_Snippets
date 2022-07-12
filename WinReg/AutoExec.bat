eeb2845e95284576c6f940491fdace150be88e5a
viveklucky1986 <viveklucky1848@yahoo.in>
2022-07-12 06:03:31 +0530

    Create AutoExec.bat

---
+++ WinReg/AutoExec.bat
@@ -0,0 +1,4 @@
+:: "C:\RR_Projects\ConfigScripts\AutoExec.bat"
+doskey subl="%ProgramFiles%\Sublime Text\sublime_text.exe" $*
+doskey sublime="%ProgramFiles%\Sublime Text\sublime_text.exe" $*
+doskey sublime-text="%ProgramFiles%\Sublime Text\sublime_text.exe" $*
